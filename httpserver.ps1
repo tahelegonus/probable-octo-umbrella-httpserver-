79c7366ff131031080453d048a6af418bb15315e
tahelegonus <telegonuscpt866@gmail.com>
2026-07-08 14:51:05 -0400

    httpservertest.ps1

---
+++ httpserver.ps1
@@ -0,0 +1,49 @@
+PS C:\Users\tahelegonus\httpserver> dir 
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----          7/8/2026  10:36 AM                .copilot
+d-----          4/1/2025   9:04 AM                .docker
+d-----          7/8/2026  10:44 AM                .dotnet
+d-----         4/28/2025  10:18 AM                .icesoft
+d-----         4/28/2025  10:17 AM                .openjfx
+d-----         1/22/2026  10:01 AM                .ssh
+d-----        12/31/2025   9:18 AM                .swt
+d-----         4/30/2025   9:24 AM                .vscode
+d-----          7/8/2026  10:21 AM                .vscode-shared
+d-r---         1/16/2025   8:04 AM                3D Objects
+d-r---         1/16/2025   8:04 AM                Contacts
+d-r---         6/26/2026   9:13 AM                Desktop
+d-r---          7/8/2026   8:02 AM                Documents
+d-r---          7/8/2026   8:07 AM                Downloads
+d-r---         1/16/2025   8:04 AM                Favorites
+d-----          7/8/2026  11:56 AM                httpserver
+d-----          7/8/2026   7:36 AM                httpserver.c
+d-r---         1/16/2025   8:04 AM                Links
+d-r---         1/16/2025   8:04 AM                Music
+d-r---         6/25/2024   4:04 PM                OneDrive
+d-r---         1/16/2025   8:04 AM                Pictures
+d-r---         1/16/2025   8:04 AM                Saved Games
+d-r---         3/25/2025   2:09 PM                Searches
+d-r---          4/2/2025  10:00 AM                Videos
+-a----          7/7/2026   1:46 PM          15952 fp
+-a----          7/7/2026   1:45 PM            101 fp.c
+-a----          7/8/2026   7:30 AM            417 index.html
+-a----          7/7/2026   2:37 PM            912 pswdchk.c
+-a----         3/27/2025   1:17 PM              0 saf-cli.log
+-a----          7/7/2026   2:50 PM            160 touch
+
+PS C:\Users\tahelegonus\httpserver> Test-NetConnection localhost -Port 8080
+
+WARNING: TCP connect to (::1 : 8080) failed
+WARNING: TCP connect to (127.0.0.1 : 8080) failed
+
+
+ComputerName           : localhost
+RemoteAddress          : ::1
+RemotePort             : 8080
+InterfaceAlias         : Loopback Pseudo-Interface 1
+SourceAddress          : ::1
+PingSucceeded          : True
+PingReplyDetails (RTT) : 0 ms
+TcpTestSucceeded       : False
